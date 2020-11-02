6547e1f2082e62fdd25592cf6743a259c831d000
R. Teodoro <26728400+r-teodoro@users.noreply.github.com>
2020-11-02 10:26:13 -0300

    Create python script to GET data from API

---
+++ HGFinance-JSON.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+import requests
+
+#Documentação:
+#   Base: https://console.hgbrasil.com/documentation/finance
+#   Lista de Símbolos: https://console.hgbrasil.com/documentation/finance/symbols
+
+url = "https://api.hgbrasil.com/finance/stock_price"
+valid_key = "" #Acesse o site do HG Brasil para obter sua chave
+
+for item in open("HGFinance-Lista.txt"):
+    ticker = item.replace('\n', '')
+    parametros = {"key":valid_key,"symbol":ticker}
+    response = requests.get(url, params=parametros)
+    content = json.loads(response.content)
+    results = content['results'][ticker]
+    atualizacao = results["updated_at"][0:10]
+   
+    print(f"A cotação atual de {results['symbol']} é {results['price']}, a variação em relação ao dia anterior é {results['change_percent']}")
+    
+    with open(os.path.join('json', f'{ticker}.json'), 'w') as file:
+        json.dump(results, file)
